a9dcb0ee5ea181ef9309187e3e73503c329bbfab
Gabriel Martínez Zarama <gabomarza@hotmail.com>
2019-03-02 23:04:19 -0500

    Actualizan pruebas, jsons y js
    Se agregan generos, fansubs con sus debidas pruebas y se modifica
    autores en lo necesario, solo se modifican js, json y los colection de
    pruebas

--- Backend/routes/autores.js
+++ Backend/routes/autores.js
@@ -43,7 +43,7 @@ router.put('/:id', function(req, res, next) {
             jsonfile.writeFile('./persistence/Autores.json', obj, function(err) {
                 if (err) throw err;
             });
-            res.send(obj);
+            res.send(obj[ind]);
         }
         else{
             res.statusCode=404;
