02f87f8936b63a6e7a59293c0917ecd1fa67a2e3
Gabriel Martínez Zarama <gabomarza@hotmail.com>
2019-03-02 19:03:51 -0500

    Agregar autores

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var novelasRouter= require('./routes/novelas');
+var autoresRouter= require('./routes/autores');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/novelas', novelasRouter);
+app.use('/autores', autoresRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
