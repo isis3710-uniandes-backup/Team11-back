c348330ac06674f2deb9279614d506035dd507e9
AMILKAR ESLAVA GALVIS <AMILKAR@sis.virtual.uniandes.edu.co>
2019-03-22 23:45:38 -0500

    faq y contacto

--- frontend/src/Components/faq.js
+++ frontend/src/Components/faq.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 const Faq = (props) => (
-    <p>faq</p>
+    <div>
+        <h1>faq</h1>
+        <h4><strong> pregunta 1</strong></h4>
+        <p>adasdghascdjbasdlbashdvnasmndkjhavsndaslkdjbasbdmashjdnaslkdhkjas dkjas dkjashdkjashkj</p>
+        <h4><strong> pregunta 1</strong></h4>
+        <p>lsf,nbnsdbf,nsdkfbsnmdfnsbfmnsdkjh ksjdf sdkjfksdf hkjsdfksdhfksdhfm</p>
+        <h4><strong> pregunta 1</strong></h4>
+        <p>lsf,nbnsdbf,nsdkfbsnmdfnsbfmnsdkjh ksjdf sdkjfksdf hkjsdfksdhfksdhfm</p>
+    </div>
+    
 );
 
 export default Faq;
