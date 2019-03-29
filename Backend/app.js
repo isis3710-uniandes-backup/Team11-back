8b1d9be0ac17e66fcaf1d56e81718cb3c23f874e
Rafael Tejón <rc.tejon@uniandes.edu.co>
2019-03-29 07:26:36 -0500

    back end

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,8 @@ app.set('view engine', 'pug');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
 });
 
 app.use(logger('dev'));
